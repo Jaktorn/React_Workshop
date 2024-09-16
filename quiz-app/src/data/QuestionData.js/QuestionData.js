8bdc4f95893a97a810165246805615f6d32bc17c
Jaktorn <jaktopn@gmail.com>
2024-09-16 10:55:38 +0700

    create quiz button

---
+++ quiz-app/src/data/QuestionData.js/QuestionData.js
@@ -0,0 +1,28 @@
+const QuestionData = [
+    {
+        question:"ข้อใดไม่ใช่ tag html",
+        A: "<react>",
+        B: "<a>",
+        C: "<b>",
+        D: "<h1>",
+        answer:"A"
+    },
+    {
+        question:"ข้อใดถูกต้องเกี่ยวกับ React",
+        A:"ทำงานฝั่ง Server",
+        B:"ใช้จัดการฐานข้อมูล",
+        C:"เป็น JavaScrips Library",
+        D:"ถูกทุกข้อ",
+        answer:"C"
+    },
+    {
+        question:"ข้อใดคือชื่อ Browser",
+        A:"PHP",
+        B:"React",
+        C:"MySQL",
+        D:"Chorme",
+        answer:"D"
+    }
+]
+
+export default QuestionData;
